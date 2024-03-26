0f6a402a92ed8b7b68526bf621ecb8320996f84d
LiZhYun <zhiyuanli@std.uestc.edu.cn>
2024-03-26 16:31:14 +0800

    script

---
+++ examples/train_mujoco_igcsac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=mujoco-igcsac
+#SBATCH --output=./out/mujoco-igcsac%A_%a.out # Name of stdout output file
+#SBATCH --error=./out/mujoco-igcsac%A_%a.txt  # Name of stderr error file
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
+#SBATCH --array=0-4
+
+env="mamujoco"
+scenario=$1
+agent_conf=$2
+
+algo="igcsac"
+exp="check"
+
+echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}"
+
+CUDA_VISIBLE_DEVICES=0 singularity exec --bind /scratch --nv /scratch/work/zhaow7/mujoco_football_triton.sif /bin/sh -c "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/zhaow7/.mujoco/mujoco210/bin; python ./train.py \
+--env ${env} --algo ${algo} --exp_name ${exp} \
+--scenario ${map} --agent_conf ${agent_conf} --seed $SLURM_ARRAY_TASK_ID --n_rollout_threads 40 \
+--num_env_steps 10000000 --eval_episodes 5"
+
